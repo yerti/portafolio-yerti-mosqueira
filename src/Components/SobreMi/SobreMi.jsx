3a1098ac9df6fa94024df52a951a9645eb05bc17
yerti <yerti0818@gmail.com>
2024-03-13 13:11:05 -0500

    change component description about me

--- src/Components/SobreMi/SobreMi.jsx
+++ src/Components/SobreMi/SobreMi.jsx
@@ -22,9 +22,9 @@ export default function SobreMi() {
       <div className='contenido-texto-sobre-mi'>
         <img className='imagen-aspiral-morado-sobre-mi' src={imagenes["img-morados"]} alt="imagen de aspirales morados" />
         <h2 className='titulo-presentacion'>Hola! <br /> Soy Yerti </h2>
-        <p>Una desarrolladora full-stack con una pasión creciente por el desarrollo frontend. Aunque mi experiencia puede ser limitada, estoy dedicada a aprender y crecer en este emocionante campo.</p> <br />
-        <p>Mi enfoque principal es crear interfaces de usuario intuitivas y atractivas que mejoren la experiencia del usuario. Mis proyectos reflejan mi compromiso con la calidad y la atención al detalle, y estoy emocionada por continuar expandiendo mi conjunto de habilidades mientras trabajo en nuevos desafíos.</p> <br />
-        <p>Si estás buscando un desarrollador frontend entusiasta y dedicado para tu próximo proyecto, ¡me encantaría ser parte de tu equipo!</p>
+        <p>Una desarrolladora full-stack con una pasión creciente por el desarrollo frontend. Aunque aún estoy en el proceso de aprender y crecer en este emocionante campo, mi dedicación y entusiasmo son inquebrantables.</p> <br />
+        <p>Mi enfoque principal radica en crear interfaces de usuario intuitivas y atractivas que mejoren significativamente la experiencia del usuario. Cada uno de mis proyectos refleja mi compromiso con la calidad y la atención al detalle, y estoy emocionada por seguir ampliando mi conjunto de habilidades mientras me enfrento a nuevos y emocionantes desafíos.</p> <br />
+        <p>Si estás buscando un miembro entusiasta y dedicado para tu equipo de desarrollo frontend, ¡me encantaría ser parte de él y contribuir con mi energía y pasión al éxito de tu próximo proyecto!</p>
       </div>
     </div>
   )
